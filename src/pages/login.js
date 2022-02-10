18b1259638222fe1d84191791b6c956dba6b2844
Kassem <systemry420@gmail.com>
2022-02-10 19:38:49 +0200

    basic structure

---
+++ src/pages/login.js
@@ -0,0 +1,17 @@
+import React from "react";
+import '../index.css';
+
+
+function Login() {
+  return (
+    <div className="container">
+        <form className="col-12">
+            
+
+            
+        </form>
+    </div>
+  );
+}
+
+export default Login;
