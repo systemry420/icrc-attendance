54ebb2386dd8efaad6d94a54f9b59633c793fde4
Kassem <systemry420@gmail.com>
2022-02-13 19:22:02 +0200

    add team member component

--- src/pages/admin/Team.js
+++ src/pages/admin/Team.js
@@ -1,32 +1,62 @@
 import React, { useState } from "react";
 
 function Team() {
+  const [showForm, setShowForm] = useState(false);
+  const [teamList, setTeamList] = useState([]);
 
+  const addMember = (member) => {
+    setTeamList([...teamList, member])
+    console.log(teamList);
+  };
 
   return (
     <div className="container p-4">
       <div className="fill-form form-box">
         <div className="row">
-          <div className="col-6">
+          <div className="col-8">
             <h1>Add Member</h1>
           </div>
-          <div className="col-6">
-            <input style={{'font-size': '2em', 'padding': '0', 'width': '1.5em'}} type="button" value="+" />
+          <div className="col-4">
+            <button 
+              onClick={() => setShowForm(!showForm)}
+              style={{
+                'fontSize': '2em',
+                'height': '1.5em',
+                'padding': '0',
+                'width':'1.5em',
+                'border': 'none',
+                'alignItems': 'center',
+                'alignContent': 'center',
+                'verticalAlign': 'middle',
+                'borderRadius': '50%'
+                }}>
+                  <span  style={{'height': '0', 'padding': '0'}}>+</span> 
+                </button>
           </div>
+          {
+            showForm ? (
+              <Form className='col-6' addMember={addMember} />
+            ) : ('')
+          }
 
-          <Form />
+          <List className='col-6' teamList={teamList} />
         </div>
-       
       </div>
     </div>
   );
 }
 
-const Form = () => {
+const Form = ({ addMember }) => {
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
 
-  const handleSubmit = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const member = {name, phone};
+    addMember(member);
+    setName('')
+    setPhone('')
+  }
 
   return (
     <form className="col-lg-6 col-md-6 col-sm-12">
@@ -57,7 +87,28 @@ const Form = () => {
   )
 }
 
-const List = () => {
-  
+const List = ({ teamList }) => {
+  return (
+    <div className="col-lg-6 col-md-6 col-sm-12">
+        <hr style={{'margin': '1em'}}/>
+       <h1>List</h1>
+        {teamList.length > 0 ? (
+          <ul className='list-group'>
+            {teamList.map((member, idx) => {
+              return (
+                <li 
+                    className="py-3 list-group-item d-flex justify-content-between" 
+                    key={idx}>
+                  {member.name}
+                  <span className='text-danger'>X</span>
+                </li>
+              );
+            })}
+          </ul>
+        ) : (
+          <h3>No member has been added</h3>
+        )}
+    </div>
+  )
 }
 export default Team;
